ca61b5851010ee143e56e3314d9b6c0f237e3d3d
mars_salada12 <gonzalezbeltranmarcela@gmail.com>
2024-05-13 14:49:33 -0600

    prueba 2

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+interface FormData {
+  nombre: string;
+  email: string;
+  mensaje: string;
+}
+
+const App: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    nombre: '',
+    email: '',
+    mensaje: '',
+  });
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
+
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   // Aquí puedes realizar acciones adicionales con los datos del formulario
+  //   console.log(formData);
+  //   // Restablecer el formulario después de enviarlo
+  //   setFormData({ nombre: '', email: '', mensaje: '' });
+  // };
+
+  return (
+    <div className=" " >
+      <form //onSubmit={handleSubmit}
+      >
+
+        <div>
+          <label htmlFor="nombre">Nombre: </label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={formData.nombre}
+            //onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            //onChange={handleChange}
+          />
+        </div>
+       
+        <button className= "justify-center" type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default App
